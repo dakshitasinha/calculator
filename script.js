a377047597e72c85f16b7ce78b00ca689e5c30bd
dakshitasinha2019@gmail.com <dakshitasinha2019@gmail.com>
2026-08-04 00:35:08 +0530

    feat: add two-line calculator display with history

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let runningTotal=0;
-let buffer="0";
-let previousOperator;
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
 
-const screen = document.querySelector('.screen');
+// NEW
+let history = "";
+
+const screen = document.querySelector(".screen");
+const historyScreen = document.querySelector(".history");
 
 function buttonClick(value){
   if(isNaN(value)){
@@ -10,23 +14,33 @@ function buttonClick(value){
   } else{
     handleNumber(value);
   }
-  screen.innerText=buffer;
+ screen.innerText = buffer;
+historyScreen.innerText = history;
 }
 
 function handleSymbol(symbol){
   switch(symbol){
     case 'C':
-      buffer="0";
-      runningTotal=0;
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      history = "";
       break;
     case '=':
-      if(previousOperator===null){
+      if(previousOperator === null){
         return;
       }
+
+      history += " " + buffer;
+
       flushOperation(parseInt(buffer));
-      previousOperator=null;
-      buffer=runningTotal;
-      runningTotal=0;
+
+      previousOperator = null;
+      buffer = runningTotal.toString();
+
+      history = "";
+
+      runningTotal = 0;
       break;
     case '←':
       if(buffer.length===1){
@@ -45,19 +59,23 @@ function handleSymbol(symbol){
 }
 
 function handleMath(symbol){
-  if(buffer==="0"){
+  if(buffer === "0"){
     return;
   }
 
-  const intBuffer=parseInt(buffer);
+  const intBuffer = parseInt(buffer);
 
-  if(runningTotal===0){
-    runningTotal=intBuffer;
+  if(runningTotal === 0){
+    runningTotal = intBuffer;
   } else{
     flushOperation(intBuffer);
   }
-  previousOperator=symbol;
-  buffer="0";
+
+  previousOperator = symbol;
+
+  history = runningTotal + " " + symbol;
+
+  buffer = "0";
 }
 
 function flushOperation(intBuffer){
